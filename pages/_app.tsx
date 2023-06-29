a81c25794789f4c51c612cb871eacfb02cfccfaf
guangzhengli <iguangzhengli@gmail.com>
2023-06-29 22:07:40 +0800

    feat: add next auth session provider.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import '@/styles/globals.css';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
+import {SessionProvider} from "next-auth/react";
 
 const inter = Inter({ subsets: ['latin'] });
 
-function App({ Component, pageProps }: AppProps<{}>) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <main className={inter.className}>
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </main>
   );
 }
