54756e78bfa7401947c4297c3e88915d8f9a07bd
guangzhengli <iguangzhengli@gmail.com>
2023-07-05 21:44:28 +0800

    feat: modify dark IndexFormTabs style.

--- components/Chat/IndexFormTabs.tsx
+++ components/Chat/IndexFormTabs.tsx
@@ -19,40 +19,42 @@ export const IndexFormTabs = (
   }: Props) => {
   return (
     <>
-      <Tabs defaultValue="fileloader" className="w-full">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="fileloader">FileLoader</TabsTrigger>
-          <TabsTrigger value="webloader">WebLoader</TabsTrigger>
-        </TabsList>
-        <TabsContent value="fileloader">
-          <Card>
-            <CardHeader>
-              <CardTitle className="ml-10">FileLoader</CardTitle>
-              <CardDescription className="ml-10">
-                Use File to upload your vector.
-              </CardDescription>
-            </CardHeader>
-            <FileLoaderForm keyConfiguration={keyConfiguration}
-                            handleKeyConfigurationValidation={handleKeyConfigurationValidation}
-                            handleShowIndexFormTabs={handleShowIndexFormTabs} />
-          </Card>
-        </TabsContent>
-        <TabsContent value="webloader">
-          <Card>
-            <CardHeader>
-              <CardTitle>WebLoader</CardTitle>
-              <CardDescription>
-                Use Web page to upload your vector.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="current">Coming Soon....</Label>
-              </div>
-            </CardContent>
-          </Card>
-        </TabsContent>
-      </Tabs>
+      <div className="bg-white dark:bg-[#343541] w-full">
+        <Tabs defaultValue="fileloader">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="fileloader">FileLoader</TabsTrigger>
+            <TabsTrigger value="webloader">WebLoader</TabsTrigger>
+          </TabsList>
+          <TabsContent className="mt-2" value="fileloader">
+            <Card>
+              <CardHeader>
+                <CardTitle className="ml-10">FileLoader</CardTitle>
+                <CardDescription className="ml-10">
+                  Use File to upload your vector.
+                </CardDescription>
+              </CardHeader>
+              <FileLoaderForm keyConfiguration={keyConfiguration}
+                              handleKeyConfigurationValidation={handleKeyConfigurationValidation}
+                              handleShowIndexFormTabs={handleShowIndexFormTabs}/>
+            </Card>
+          </TabsContent>
+          <TabsContent className="mt-2" value="webloader">
+            <Card>
+              <CardHeader>
+                <CardTitle>WebLoader</CardTitle>
+                <CardDescription>
+                  Use Web page to upload your vector.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="current">Coming Soon....</Label>
+                </div>
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
     </>
   )
 }
