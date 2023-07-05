28505bf80a640376873ca5b93588823d2a508dde
guangzhengli <iguangzhengli@gmail.com>
2023-07-05 21:59:26 +0800

    feat: add update user likes api.

--- pages/api/indexes/user/[id]/index.ts
+++ pages/api/indexes/user/[id]/index.ts
@@ -1,11 +1,17 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { id } = req.query;
+    const session = await getServerSession(req, res, authOptions);
+    if (!session?.user?.id) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
 
     const indexes = await prisma?.index.findMany({
         where: {
-            authorId: id as string,
+            authorId: session?.user?.id as string,
         },
         select: {
             id: true,
--- pages/api/indexes/user/[id]/likes.ts
+++ pages/api/indexes/user/[id]/likes.ts
@@ -1,11 +1,17 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { id } = req.query;
+const handleGet = async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session?.user?.id) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
 
     const user = await prisma?.user.findUnique({
         where: {
-            id: id as string,
+            id: session?.user?.id as string,
         },
         select: {
             likedIndexIds: true,
@@ -30,6 +36,43 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     });
 
     res.status(200).json(indexes);
+}
+
+const handlePut = async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getServerSession(req, res, authOptions);
+    const { indexIds } = req.query;
+
+    if (!session?.user?.id) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
+
+    if (!indexIds || indexIds.length === 0) {
+        res.status(400).json({ message: "No index IDs provided" });
+        return;
+    }
+
+    const updatedData = await prisma?.user.update({
+        where: { id: session?.user?.id as string },
+        data: {
+            likedIndexIds: {
+                set: indexIds as string[],
+            }
+        }
+    });
+
+    res.status(200).json(updatedData);
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    switch (req.method) {
+        case 'GET':
+            return handleGet(req, res);
+        case 'POST':
+            return handlePut(req, res);
+        default:
+            return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
 }
 
 export default handler;
