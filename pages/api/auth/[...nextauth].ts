e64b59fb906a223ac10f44bfc82b32703b4cc2cb
guangzhengli <iguangzhengli@gmail.com>
2023-07-09 17:43:03 +0800

    feat: add google oauth2 provider support.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from "next-auth/providers/google";
 import {prisma} from '@/lib/prisma';
 import {AuthOptions} from "next-auth/src";
 
@@ -15,6 +16,10 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    })
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.AUTH_SECRET,
