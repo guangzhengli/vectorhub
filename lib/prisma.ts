8e09689a86c5fe9b6f5599b6b9b39b5c9d64c226
guangzhengli <iguangzhengli@gmail.com>
2023-07-10 20:00:20 +0800

    feat: fix the prisma client in prod.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
 declare global {
-  var prisma: PrismaClient | undefined
+  var prisma: PrismaClient
 }
 
 export const prisma =
   global.prisma ||
   new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+global.prisma = prisma
