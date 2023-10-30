4ed804f8470007dabeb6529a577f7caed9132a74
Vincenzo Prencipe <v.prencipe@reply.it>
2023-10-30 09:05:51 +0100

    first deploy

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,32 +7,32 @@ import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from "next-auth/providers/google";
 import {prisma} from '@/lib/prisma';
 import {AuthOptions} from "next-auth";
+import CognitoProvider from "next-auth/providers/cognito";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
 
+// @ts-ignore
+// @ts-ignore
 export const authOptions: AuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    })
-  ],
+  providers: [CognitoProvider({
+    clientId: process.env.COGNITO_CLIENT_ID!,
+    clientSecret: process.env.COGNITO_CLIENT_SECRET!,
+    issuer: process.env.COGNITO_ISSUER
+  })],
   adapter: PrismaAdapter(prisma),
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_AUTH_SECRET, //new secrets
   callbacks: {
     session: async ({ session, token }: { session: any, token: any }) => {
       if (session?.user) {
         session.user.id = token.sub;
       }
       return session;
-    },
+    }
   },
   session: {
     strategy: 'jwt',
   },
 }
+
+//ADDED COGNITO PROVIDER
 export default authHandler;
